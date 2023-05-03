8c5f515ce9e9b3283cccd5fb18eaa30130bc1ce7
Anshul ojha <ojhaanshul889@gmail.com>
2023-05-03 21:25:41 +0530

    second c

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const mongdb = require("./db")
-const app = express()
-const port = 5000
+const express = require("express");
+const mongdb = require("./db");
+const app = express();
+const port = 5000;
 mongdb();
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://ephemeral-brioche-8ff192.netlify.app/"
+    "https://tame-tan-betta-cap.cyclic.app"
   );
   res.header(
     "Access-control-Allow-Headers",
@@ -16,15 +16,15 @@ app.use((req,res,next)=>{
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use(express.json());
-app.use('/api',require("./Routes/CreatUser"));
-app.use('/api',require("./Routes/DisplayData"));
-app.use('/api',require("./Routes/OrderData"));
+app.use("/api", require("./Routes/CreatUser"));
+app.use("/api", require("./Routes/DisplayData"));
+app.use("/api", require("./Routes/OrderData"));
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 /*
 https://tame-tan-betta-cap.cyclic.app */
