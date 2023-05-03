538390126953ceb6210518caeeb4f6df17fe5c4a
Anshul ojha <ojhaanshul889@gmail.com>
2023-05-03 22:38:12 +0530

    second c

--- Routes/DisplayData.js
+++ Routes/DisplayData.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const mongdb = require("./db");
-mongdb();
 router.post('/foodData',(req,res)=>{
     try{
         res.send([global.foodData2,global.foodCategory])
