4b0f16d098c3b3ac04c0adf553983bac88e2ec87
Anshul ojha <ojhaanshul889@gmail.com>
2023-05-08 22:35:18 +0530

    dd

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongdb = require("./db");
 const app = express();
 const port = 5000;
-
-mongoose
-  .connect(
-    "mongodb+srv://Anshul_ojha:Luhsna@atlascluster.ekt7t1o.mongodb.net/mern_db?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
-  .then(async (result) => {
-    console.log("Mongo connected");
-    const fetched_data = await result.connection.db.collection("foodData2");
-
-    fetched_data
-      .find({})
-      .toArray()
-      .then(async (data) => {
-        const foodCategory = await result.connection.db.collection(
-          "foodCategory"
-        );
-        foodCategory
-          .find({})
-          .toArray()
-          .then((catData) => {
-            global.foodData2 = data;
-            global.foodCategory = catData;
-          });
-      });
-  });
+mongdb();
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -41,11 +16,11 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.use(express.json());
 app.use("/api", require("./Routes/CreatUser"));
 app.use("/api", require("./Routes/DisplayData"));
 app.use("/api", require("./Routes/OrderData"));
-
-app.listen(process.env.PORT || 5000, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 /*
