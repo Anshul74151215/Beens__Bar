7306d43c66e0200a1acbdc2a9abafd144cf56fb6
Anshul ojha <ojhaanshul889@gmail.com>
2023-05-20 18:55:22 +0530

    f

--- Routes/DisplayData.js
+++ Routes/DisplayData.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router();
 router.get('/foodData',(req,res)=>{
     try{
-        req.send([global.foodData2,global.foodCategory])
+        res.send([global.foodData2,global.foodCategory])
 
     }catch(error){
         console.log(error,message);
-        req.send("server err...")
+        res.send("server err...")
     }
 })
 module.exports = router;
